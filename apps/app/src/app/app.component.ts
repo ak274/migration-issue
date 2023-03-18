e56323ec0f614960833914eb4a1552d0ec217155
Ankush Kalia <ankush.kalia@ihsmarkit.com>
2023-03-18 19:07:47 +0530

    Initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'migration-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+}
